d2d7bd31cd0f484f5a6c6d90e5f73476540fc40c
Alex Mitchell <alex@alexthemitchell.com>
2017-03-06 14:56:12 -0600

    Add first pass of newSemester function

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,4 +2,13 @@ class DashboardController < ApplicationController
   def get
     @semesters = Semester.all
   end
+
+  def post
+    if params[:action] == "newSemester"
+      if params[:year] and params[:term]
+        Semeseter.new(academic_year: params[:year], academic_term: params[:term])
+      end
+    end
+    redirect_to get
+  end
 end
