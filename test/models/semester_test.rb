70e11200d6338460dd88d5131837816585ac07c2
Prabir Pradhan <prabirmsp@gmail.com>
2017-03-07 17:17:10 -0600

    add tests and fixtures for semester and course

--- test/models/semester_test.rb
+++ test/models/semester_test.rb
@@ -2,7 +2,23 @@ require 'test_helper'
 require 'minitest/autorun'
 
 class SemesterTest < ActiveSupport::TestCase
-  def test_test
-    assert_equal 2, Semester.count
+  test "count of fixtures" do
+    assert_equal 3, Semester.count # loaded from fixtures
+  end
+
+  test "validity of fixtures" do
+    sems = Semester.all
+    sems.each do |sem|
+      assert sem.valid?
+    end
+  end
+
+  test "validity when saving new semester" do
+    sem = Semester.new
+    assert_not sem.valid?
+    sem.academic_year= "2018/2019"
+    assert_not sem.valid?
+    sem.academic_term= "Fall"
+    assert sem.valid?
   end
 end
