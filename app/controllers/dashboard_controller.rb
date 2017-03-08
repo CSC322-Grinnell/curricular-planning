9209496b50bd05cd6526bee6bc149cef58fa4c5f
Alex Mitchell <alex@alexthemitchell.com>
2017-03-08 14:54:53 -0600

    Add POST handling

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,17 @@
 class DashboardController < ApplicationController
+
   def get
     @semesters = Semester.all
   end
 
   def post
-    if params[:action] == "newSemester"
+    puts params
+    if params[:request] == "newSemester"
       if params[:year] and params[:term]
-        Semeseter.new(academic_year: params[:year], academic_term: params[:term])
+        @semester = Semester.new(academic_year: params[:year], academic_term: params[:term])
+        @semester.save
       end
     end
-    redirect_to get
+    redirect_to "/dashboard"
   end
 end
