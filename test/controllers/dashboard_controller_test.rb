2d6dd73d66aa5f098e2da45fd1990604e2ebd643
Prabir Pradhan <prabirmsp@gmail.com>
2017-03-15 19:53:04 +0000

    add test case

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "post create new course" do
+    initialNumberCourses = Course.count
+    post :dashboard, 
+      request: "newCourse",
+      course_number: "161", 
+      title: "Functional", 
+      description: "This is the description",
+      required: true
+    assert_response :ok
+    assert_equal initialNumberCourses + 1, Course.count
+  end
+  
 end
