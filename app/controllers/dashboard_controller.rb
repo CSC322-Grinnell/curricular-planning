f7bd56a4f1ef92fca6606122c91ed75509897c75
Prabir Pradhan <prabirmsp@gmail.com>
2017-03-15 19:36:20 +0000

    refactor dashbord controller and add new course post handler

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,12 +4,35 @@ class DashboardController < ApplicationController
   end
 
   def post
-    if params[:request] == "newSemester"
-      if params[:year] and params[:term]
-        @semester = Semester.new(academic_year: params[:year], academic_term: params[:term])
-        @semester.save
-      end
+    case params[:request]
+    when "newSemester"
+      handleNewSemester
+    when "newCourse"
+      handleNewCourse
     end
     redirect_to "/dashboard"
   end
+  
+  private 
+  
+  def handleNewSemester
+    if params[:year] and params[:term]
+      @semester = Semester.new(academic_year: params[:year], academic_term: params[:term])
+      @semester.save
+    end
+  end
+  
+  def handleNewCourse
+    if params[:course_number] \
+      and params[:title] \
+      and params[:description] \
+      and params[:required]
+      course = Course.new(course_number: params[:course_number],
+                          title: params[:title],
+                          description: params[:description],
+                          required: params[:required])
+      course.save if course.valid?
+    end
+  end
+  
 end
