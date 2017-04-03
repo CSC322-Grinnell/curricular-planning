87bd3078c6816d57da3d99ad34be876be07bb1d7
Alex Mitchell <alex@alexthemitchell.com>
2017-04-03 13:29:27 -0500

    update gemfile lock

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  tests DashboardController
+
+  test "A properly formatted POST request to the DashboardController should create a corresponding semester" do
+
+    post(:post, 
+    assert_response :found
+    assert_not_nil Semester.find_by(
+  
+  end
 end
+
