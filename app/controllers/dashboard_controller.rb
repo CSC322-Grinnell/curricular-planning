4ee82c9a1b8757803d05571009e74a58e8a00df8
Otabek Nazarov <nazarovo@grinnell.edu>
2017-04-05 19:52:56 +0000

    need to troubleshoot

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,12 +4,34 @@ class DashboardController < ApplicationController
   end
 
   def post
-    if params[:request] == "newSemester"
-      if params[:year] and params[:term]
-        @semester = Semester.new(academic_year: params[:year], academic_term: params[:term])
-        @semester.save
-      end
+    case params[:request]
+    when "newSemester"
+      handleNewSemester
+    when "newCourse"
+      handleNewCourse
     end
     redirect_to "/dashboard"
   end
+
+  private
+
+  def handleNewSemester
+    if params.require(:year) and params.require(:term)
+      semester = Semester.new(academic_year: params[:year], academic_term: params[:term])
+      semester.save if semester.valid?
+    end
+  end
+
+  def handleNewCourse
+    requiredParams = [:course_number, :title, :description]
+    # check if all requried parameters are present
+    if requiredParams.reduce(true) {|val,x| val and params.require(x)}
+      course = Course.new(course_number: params[:course_number],
+                          title: params[:title],
+                          description: params[:description],
+                          required: params[:required])
+      course.save if course.valid?
+    end
+  end
+
 end
