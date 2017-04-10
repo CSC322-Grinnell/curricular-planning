a6675aa0c3792cd402aa2bd069eeea4057a3ded7
Prabir Pradhan <prabirmsp@gmail.com>
2017-04-10 19:55:16 +0000

    begin work on dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -13,71 +13,71 @@
 
 ActiveRecord::Schema.define(version: 20170405193701) do
 
-  create_table "admins", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+  create_table "admins", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
-  create_table "courses", force: :cascade do |t|
-    t.string   "course_number", limit: 255
-    t.string   "title",         limit: 255
+  create_table "courses", force: true do |t|
+    t.string   "course_number"
+    t.string   "title"
     t.text     "description"
     t.boolean  "required"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
-  create_table "interests", id: false, force: :cascade do |t|
+  create_table "interests", id: false, force: true do |t|
     t.integer "user_id",     null: false
     t.integer "offering_id", null: false
   end
 
-  create_table "offerings", force: :cascade do |t|
-    t.string   "professor",   limit: 255
-    t.string   "time",        limit: 255
+  create_table "offerings", force: true do |t|
+    t.string   "professor"
+    t.string   "time"
     t.integer  "capacity"
     t.integer  "course_id"
     t.integer  "semester_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_index "offerings", ["course_id"], name: "index_offerings_on_course_id"
   add_index "offerings", ["semester_id"], name: "index_offerings_on_semester_id"
 
-  create_table "semesters", force: :cascade do |t|
-    t.string   "academic_year", limit: 255
-    t.string   "academic_term", limit: 255
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+  create_table "semesters", force: true do |t|
+    t.string   "academic_year"
+    t.string   "academic_term"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
