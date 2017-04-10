1ca8e16b60cb4b9b5a7ef7472efceb187b758dca
Marija Ivica <ivicamar@grinnell.edu>
2017-04-10 19:51:33 +0000

    m

--- db/schema.rb
+++ db/schema.rb
@@ -11,29 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170408234710) do
-=======
-ActiveRecord::Schema.define(version: 20170405193701) do
-
-  create_table "admins", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
->>>>>>> master
 
   create_table "courses", force: :cascade do |t|
     t.string   "course_number", limit: 255
@@ -62,7 +40,6 @@ ActiveRecord::Schema.define(version: 20170405193701) do
   add_index "offerings", ["course_id"], name: "index_offerings_on_course_id"
   add_index "offerings", ["semester_id"], name: "index_offerings_on_semester_id"
 
-<<<<<<< HEAD
   create_table "roles", force: true do |t|
     t.string   "name"
     t.integer  "resource_id"
@@ -79,13 +56,6 @@ ActiveRecord::Schema.define(version: 20170405193701) do
     t.string   "academic_term"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-=======
-  create_table "semesters", force: :cascade do |t|
-    t.string   "academic_year", limit: 255
-    t.string   "academic_term", limit: 255
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
->>>>>>> master
   end
 
   create_table "users", force: :cascade do |t|
