ba0a11eba95e35ac5e81bfea8224b45daf44a3a9
Marija Ivica <ivicamar@grinnell.edu>
2017-04-12 19:09:37 +0000

    m

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,10 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.has_role?(:admin)
-      can :manage, :all
+      can [:edit, :delete, :new, :create, :update, :destroy], [Course, Offering, Semester] 
+      can :read, :all
     else
+   # can [:edit, :delete, :new, :create, :update, :destroy], [Enrollment]
       can :read, :all
     end
   end
