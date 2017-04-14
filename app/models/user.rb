195fe1659dc4d4d06695745b9391ab07c3ef8756
Otabek Nazarov <nazarovo@grinnell.edu>
2017-04-14 19:14:25 +0000

    from wanker to wanker with love

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_and_belongs_to_many :offering
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
