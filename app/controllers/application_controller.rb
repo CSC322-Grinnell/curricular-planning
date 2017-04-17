a367ff23465f4dbd9fc938f24e4563975280572d
Alex Mitchell <alex@alexthemitchell.com>
2017-04-17 14:05:22 -0500

    Fix links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     User.current_user = current_user
   end
 
+  def after_sign_in_path_for(resource)
+    get '/dashboard'
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   # See how all your routes lay out with "rake routes".
 
-  get 'home/index'
+  root 'home#index', as: :authenticated_root #homepage
 
-root 'home#index', as: :authenticated_root #homepage
-
-devise_scope :user do
-  authenticated :user do
-    get 'dashboard' => 'dashboard#get'
-    post 'dashboard' => 'dashboard#post'
+  devise_scope :user do
+      get 'dashboard' => 'dashboard#get'
+      post 'dashboard' => 'dashboard#post'
   end
-
-end
 end 
