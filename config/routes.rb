74bd3231e9443db3ef98e9b60d162e33a72057a3
Marija Ivica <ivicamar@grinnell.edu>
2017-04-17 19:47:27 +0000

    almost there

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users, controllers: { registrations: "devise/my_devise/registrations" }
   # See how all your routes lay out with "rake routes".
   get 'dashboard' => 'dashboard#get'
  post 'dashboard' => 'dashboard#post'
