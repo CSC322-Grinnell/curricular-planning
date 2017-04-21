8804e8527c2a64d073f5e51f34f499f8d8d43f57
Corey <simmonds17@grinnell.edu>
2017-04-21 19:05:48 +0000

    CRSH

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   # See how all your routes lay out with "rake routes".
-
-  get 'home/index'
-
-   root 'home#index', as: :authenticated_root #homepage
-
-  authenticated :user do
-    get 'dashboard' => 'dashboard#get'
-    post 'dashboard' => 'dashboard#post'
-  end
+  root 'home#index'
   
-
+  devise_scope :user do
+  #  root to: 'home#index'
+    
+    authenticated :user do
+       get '/dashboard' => 'dashboard#get'
+       post '/dashboard' => 'dashboard#post'
+    end
+  
+    unauthenticated :user do
+      get '/dashboard' => 'devise/sessions#new'
+      post '/dashboard' => 'dashboard#post' 
+    end
+  end 
 end
-
