800599f36b1fa798727cdc9b0159629212439947
Marija Ivica <ivicamar@grinnell.edu>
2017-04-26 04:13:31 +0000

    grinnell.edu authentication added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def set_current_user
     User.current_user = current_user
   end
-
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
