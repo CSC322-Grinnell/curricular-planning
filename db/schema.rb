a235361b0745612229fec1e1708cbbeba960c5e2
Prabir Pradhan <prabirmsp@gmail.com>
2017-04-28 19:45:22 +0000

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426181509) do
+ActiveRecord::Schema.define(version: 20170428191407) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20170426181509) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-
 
   create_table "courses", force: true do |t|
     t.string   "course_number"
