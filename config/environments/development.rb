ed552cd63e6ea741752938cdc8c43a548d203189
Marija Ivica <ivicamar@grinnell.edu>
2017-04-28 19:28:33 +0000

    email confirmation in developement

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
-  # ensures default url option in the environments files is defined
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # ensures default url option in the environments files is defined 
+  # email confirmation enabled 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 end
